01e913baf6fc28dcbc6a14ade9a3139a53f3d964
Regiane <regiane.leitte@outlook.com>
2022-10-28 15:38:56 -0300

    aprendendo javascript

---
+++ funcoes/operacoes.js
@@ -0,0 +1,53 @@
+
+//adição
+console.log(2+2);
+
+//subtração
+console.log(10-5);
+
+//multiplicação
+console.log(10*5);
+
+//Divisão
+console.log(10/2);
+
+/*
+incremento (adicionou um numero a mais na minha variavel, pq adicionei o 
+sinal ++(isso tem que ser feito na frente do nome da variável no console))
+*/
+let numero = 10;
+console.log(++numero);
+
+//decremento
+console.log(--numero);
+
+//comparação de maior
+console.log(10>5)
+
+//comparação de menor
+console.log(10<5)
+
+//comparação de menor ou igual
+console.log(10<=5)
+
+//comparação de maior ou igual
+console.log(10>=5)
+
+//comparação de igualdade
+console.log('ola' == 'ola')
+
+//comparação de igualdade rigido
+console.log(5 === "5")
+
+//comparação de diferentes(vale a mesma regra para string)
+console.log(10 != 20)
+
+//comparação de diferente rigido
+console.log(10 !== "10")
+
+//comparador logico &&
+console.log(10)
+
+
+
+
